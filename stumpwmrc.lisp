9956dd50b8b77d1bef38f666ee58c87f132455ee
Cory Koch <yunusabd786@gmail.com>
2013-07-10 10:50:11 -0400

    added mouse policy

--- stumpwmrc.lisp
+++ stumpwmrc.lisp
@@ -20,10 +20,22 @@
 (stumpwm:define-key stumpwm:*root-map* (stumpwm:kbd "C-XF86Mail") "exec claws-mail --receive-all")
 (stumpwm:define-key stumpwm:*root-map* (stumpwm:kbd "XF86Favorites") "exec synapse")
 
+;; Volume
+(stumpwm:define-key stumpwm:*root-map* (stumpwm:kbd "XF86AudioRaiseVolume") "exec amixer set Master,0 5%+")
+(stumpwm:define-key stumpwm:*root-map* (stumpwm:kbd "XF86AudioLowerVolume") "exec amixer set Master,0 5%-")
+(stumpwm:define-key stumpwm:*root-map* (stumpwm:kbd "XF86AudioMute") "exec amixer set Master toggle")
+
+;;(stumpwm:define-key stumpwm:*root-click-hook* (stumpwm:mouse "mouse-1") (gnext))
+
+;; TODO make it easier to transfer windows to groups
+
+;; input focus is transferred to the window you click on
+(setf *mouse-focus-policy* :click)
+
 ;;; Groups
 ;;; TODO can I use the CREATE macro for this?
-;; (loop for g in '("emacs" "irc" "browser" "email")
-;;      do (gnewbg g))
+;;(loop for g in '("emacs" "irc" "browser" "email")
+;;     do (gnewbg g))
 
 
 ;; TODO is there a way to eval a line in this file like C-x e in Emacs? _yes_ see stumpwm-mode.el
@@ -37,4 +49,12 @@
       (list "%g | %w                | "
 	    '(:eval (stumpwm:run-shell-command "date '+%r %a %D'" t))))
 
+;; change to stump function def
+;; (defun select-window-from-group-list ()
+;;   ())
 
+;; `C-t G'
+;;      Display all and groups windows in each group. For more information
+;;      see *note Groups::.
+;; `C-t g "'
+;;      Select a group from a list and switch to it.
